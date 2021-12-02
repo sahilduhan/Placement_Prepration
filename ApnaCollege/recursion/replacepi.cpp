5bf26ebe397859984ea34b9900eb284bc30de2c5
Sahil Duhan <74130869+sahilduhan@users.noreply.github.com>
2021-12-02 12:19:02 +0530

    git yudate

---
+++ ApnaCollege/recursion/replacepi.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void replacepi(string str)
+{
+    if (str.length() == 0)
+        return;
+    if (str[0] == 'p' && str[1] == 'i')
+    {
+        cout << "3.14";
+        replacepi(str.substr(2));
+    }
+    else
+    {
+        cout << str[0];
+        replacepi(str.substr(1));
+    }
+}
+int main()
+{
+    string val = "piaacapixpi";
+    replacepi(val);
+
+    return 0;
+}
