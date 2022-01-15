6f003b72a324aeba7bf7e224d9a5a3d391ea8952
Sahil Duhan <74130869+sahilduhan@users.noreply.github.com>
2022-01-16 01:50:44 +0530

    git udpate

--- takeUforward/graphs/isCycleDfs.cpp
+++ takeUforward/graphs/isCycleDfs.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool checkCycle(int s, int v, vector<int>adjList [], vector<int>& visited){
-
+bool checkCycle(int s, int par, vector<int>adjList [], vector<int>& visited){
+    visited[s] = 1;
+    for (auto it : adjList[s]){
+        if (!visited[it] == 0){
+            if (checkCycle(it, s, adjList, visited)) return true;
+        }
+        else if (it != par) return true;
+    }
     return false;
 }
 
@@ -10,7 +16,7 @@ bool isCycle(int v, vector<int> adjList []){
 
     for (int i = 0; i < v; i++){
         if (!visited[i]){
-            if (checkCycle(i, v, adjList, visited)) return true;
+            if (checkCycle(i, -1, adjList, visited)) return true;
         }
     }
     return false;
